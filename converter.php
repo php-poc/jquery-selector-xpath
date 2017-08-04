6424822351c3198076593ca419dd2b7c5f51f8d2
arashdalir <arashdalir@gmail.com>
2017-08-04 10:19:07 +0200

    TODOs added

--- converter.php
+++ converter.php
@@ -4,7 +4,8 @@
  * Converts a jQuery selector to its equivalent XPath, which can be used to search in DOMXPath queries or anywhere else
  * At the moment, only class and ID selectors are implemented.
  *
- * @TODO: implement all selectors which are still not implemented. @link https://api.jquery.com/category/selectors/
+ * TODO: implement all selectors which are still not implemented.
+ * @link https://api.jquery.com/category/selectors/
  *
  * @param $jquery_path
  *
@@ -13,42 +14,65 @@
 function convert_to_xpath($jquery_path)
 {
 	$converted = array();
-	$children = "";
+	$descendant = "";
 	$element = "";
 
-	$items = preg_split("/([\.|\#|\:|\[|\s+]?\w+)/", $jquery_path, -1, PREG_SPLIT_DELIM_CAPTURE|PREG_SPLIT_NO_EMPTY);
+	$items = preg_split("/([\+|\~|\,|\>|\.|\#|\:|\[|\s+]?\w+)/", $jquery_path, -1, PREG_SPLIT_DELIM_CAPTURE|PREG_SPLIT_NO_EMPTY);
 
 	foreach($items as $index => &$item)
 	{
 		switch($item[0])
 		{
 		case ':':
+			// TODO: implement pseudo selectors
 			break;
 
 		case ".":
+			// class selector
 			$class = substr($item, 1);
 			$converted[] = "contains(concat(' ', normalize-space(@class), ' '), ' {$class} ')";
 			break;
 
 		case "[":
+			//TODO: implement attribute selector
 			break;
 
 		case "#":
+			//ID selector
 			$id = substr($item, 1);
 			$converted[] = "@id='{$id}'";
 			break;
 
 		case " ";
+			//Descendant selector
 			$item = substr($item, 1);
-			$children = convert_to_xpath(join("", array_slice($items, $index)));
+			$descendant = convert_to_xpath(join("", array_slice($items, $index)));
 			break;
 
+		case ",":
+			//TODO: implement multiple selector
+			break;
+
+		case ">":
+			//TODO: implement child selector
+			break;
+
+		case "+":
+			//TODO: implement next adjacent selector
+			break;
+
+		case "~":
+			//TODO: implement next sibling selector
+			break;
+
+
 		default:
+			//element (when beginning with an alphanumeric character
 			$element = $item;
 			break;
 		}
 
-		if($children)
+		if($descendant)
 		{
 			break;
 		}
@@ -63,6 +87,5 @@ function convert_to_xpath($jquery_path)
 		$converted = "";
 	}
 
-	return "/".$element.$converted.$children;
-	//return "//div[contains(concat(' ', normalize-space(@class), ' '), ' {$jquery_path} ')]";
+	return "/".$element.$converted.$descendant;
 }
